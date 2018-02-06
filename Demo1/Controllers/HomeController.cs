6b5ea50a45c27487acb4caf17308d548d997cc70
dsolovay <dan.solovay@velir.com>
2018-02-05 21:58:24 -0500

    Add Demo1 code and tests

---
+++ Demo1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+
+namespace Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var cookie = Request.Cookies["visit_count"];
+            int i;
+            if (cookie == null)
+            {
+                Response.Cookies.Add(new HttpCookie("visit_count", "1"));
+            } else if (Int32.TryParse(cookie.Value, out i))
+            {
+                Response.Cookies.Add(new HttpCookie("visit_count", (i+1).ToString()));
+            }
+      
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Demo1Tests/HomeControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Demo1.Controllers;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Demo1Tests
+{
+    public class HomeControllerTests
+    {
+        private HomeController _homeController;
+        private HttpContextBase _httpContextBase;
+
+        public HomeControllerTests()
+        {
+            _homeController = new HomeController();
+            _httpContextBase = Substitute.For<HttpContextBase>();
+
+            _httpContextBase.Request.Cookies.Returns(new HttpCookieCollection());
+            _httpContextBase.Response.Cookies.Returns(new HttpCookieCollection());
+
+            _homeController.ControllerContext = new ControllerContext(_httpContextBase,new RouteData(), _homeController);
+        }
+
+
+        [Fact]
+        public void AboutPage_NoCookie_SetsToOne()
+        { 
+            //act 
+            _homeController.About();
+
+            //asert
+            _httpContextBase.Response.Cookies["visit_count"].Value.Should().Be("1");
+        }
+
+        [Fact]
+        public void AboutPage_HasValue_Increments()
+        {
+            //arrange
+            var cookie = new HttpCookie("visit_count", "1");
+            _httpContextBase.Request.Cookies.Add(cookie);
+
+            //act 
+            _homeController.About();
+
+            //asert
+            _httpContextBase.Response.Cookies["visit_count"].Value.Should().Be("2");
+        }
+    }
+}
